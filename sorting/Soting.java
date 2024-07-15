40216b99346d95e0a923b4abc30bb55dceb53b17
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-07-15 21:21:49 +0530

    Update Soting.java

--- sorting/Soting.java
+++ sorting/Soting.java
@@ -5,5 +5,17 @@ public class Soting {
         System.out.println("Sorting");
     }
 
+    public static void InsertionSort(int arr[]){
+        for(int i = 0;i<=arr.length-1;i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]= key;
+        }
+    }
+
     
 }
