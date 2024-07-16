6547b9671966d68197bed15eaa1e0ad63b118309
Shubham <ss9971768704@gmail.com>
2024-07-16 12:07:18 +0530

    insersion sort

--- sorting/Soting.java
+++ sorting/Soting.java
@@ -2,8 +2,39 @@
 public class Soting {
 
     public static void main(String[] args) {
-        System.out.println("Sorting");
+        int arr1[]={2,9,5,8,1,6,3,2};
+        //System.out.println("Sorting");
+        InsertionSortDecresingOrder(arr1);
+        for (int i : arr1) {
+            System.out.print(i+",");
+        }
     }
+
+    public static void InsertionSortIncresingOrder(int arr[]){
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j>=0  && arr[j]>key) {
+                arr[j+1]= arr[j];
+                j--;
+            }
+            arr[j+1]= key;
+        }
+    } 
+
+    public static void InsertionSortDecresingOrder(int arr[]){
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j>=0  && arr[j]<key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1]= key;
+        }
+    } 
 
     
 }
